dac501b75aeb500b9a62352246d96a7e56adb3c0
Kiren Raj <iamkiren@gmail.com>
2024-11-28 12:03:41 +0530

    Header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 function App() {
-  return (
-    <>
-      <h1>Hello Start</h1>
-    </>
-  );
+	return (
+		<>
+			<Header />
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+import chefLogo from "../../../public/images/chef-claude-icon.png";
+
+const Header = () => {
+	return (
+		<header>
+			<img src={chefLogo} alt="logo" />
+			<h1>Chef Claude</h1>
+		</header>
+	);
+};
+
+export default Header;
