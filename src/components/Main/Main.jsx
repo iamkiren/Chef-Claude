b832ba967ee8dd9c3b7e97c79e6b621ef8b9f904
Kiren Raj <iamkiren@gmail.com>
2024-11-30 11:00:10 +0530

    Before react 19 setup

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,16 +2,43 @@ import React from "react";
 import "./Main.css";
 
 const Main = () => {
+	const [ingredients, setIngredients] = React.useState([
+		"Chicken",
+		"Oregano",
+		"Tomatoes",
+	]);
+
+	const ingredientsListItems = ingredients.map((ingredient) => (
+		<li key={ingredient}>{ingredient}</li>
+	));
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		const newIngredient = formData.get("ingredient");
+
+		const newIngredients = [...ingredients, newIngredient];
+		setIngredients(newIngredients);
+	}
+
+	function handleAddIngredients(formData) {
+		const newIngredient = formData.get("ingredient");
+		const newIngredients = [...ingredients, newIngredient];
+		setIngredients(newIngredients);
+	}
+
 	return (
 		<main>
-			<form className="add-ingredient-form">
+			<form className="add-ingredient-form" action={handleAddIngredients}>
 				<input
 					type="text"
 					placeholder="e.g. oregano"
 					aria-label="Add Ingredient"
+					name="ingredient"
 				/>
 				<button type="button">Add ingredients</button>
 			</form>
+			<ul>{ingredientsListItems}</ul>
 		</main>
 	);
 };
